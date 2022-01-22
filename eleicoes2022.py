2765305d83704a7e7314eafe710c4be2e7e1bf97
Tiago Vinhoza <tiago.vinhoza@gmail.com>
2022-01-22 23:01:00 +0000

    update jan 22 pitagorica poll

--- eleicoes2022.py
+++ eleicoes2022.py
@@ -7,7 +7,7 @@ from matplotlib.ticker import PercentFormatter
 
 
 st.title('Eleições Legislativas 2022')
-st.caption('Atualizado: 21/1/2022 - 22h00' )
+st.caption('Atualizado: 22/1/2022 - 23h00' )
 
 DETAILED_URL = ('./detailed_district.csv')
 TOTAL_URL = ('./sim_df.csv')
