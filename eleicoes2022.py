dbbb1f451734388c38b41c365856de9a4d486258
Tiago Vinhoza <tiago.vinhoza@gmail.com>
2022-01-25 11:56:10 +0000

    add jan 25 aximage update

--- eleicoes2022.py
+++ eleicoes2022.py
@@ -7,7 +7,7 @@ from matplotlib.ticker import PercentFormatter
 
 
 st.title('Eleições Legislativas 2022')
-st.caption('Atualizado: 24/1/2022 - 22h00' )
+st.caption('Atualizado: 25/1/2022 - 12h00' )
 
 DETAILED_URL = ('./detailed_district.csv')
 TOTAL_URL = ('./sim_df.csv')
