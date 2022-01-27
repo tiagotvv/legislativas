a199d495445a90369defae85fc9dac968328490c
Tiago Vinhoza <tiago.vinhoza@gmail.com>
2022-01-27 20:39:58 +0000

    add 3 new polls 27 jan

--- eleicoes2022.py
+++ eleicoes2022.py
@@ -7,7 +7,7 @@ from matplotlib.ticker import PercentFormatter
 
 
 st.title('Eleições Legislativas 2022')
-st.caption('Atualizado: 27/1/2022 - 18h45' )
+st.caption('Atualizado: 27/1/2022 - 20h45' )
 
 DETAILED_URL = ('./detailed_district.csv')
 TOTAL_URL = ('./sim_df.csv')
